92ae32ac1f2247c8031dce22dce0c46b1d03270b
Sebastien Collette <sco@synapsis-group.com>
2022-07-12 15:49:53 +0200

    Final.

--- install.sh
+++ install.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-export DEBIAN_FRONTEND=noninteractive
-export USERNAME=`whoami`
-
-sudo apt-get install -y \
-  fonts-firacode
-
-sudo chsh -s /usr/bin/zsh $USERNAME
 cp machine-settings.json /home/codespace/.vscode-remote/data/Machine/settings.json
-cp .zshrc ~/.zshrc
