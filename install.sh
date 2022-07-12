60d8c65b5bee99c2c4f416cf61ee6eeb6c9dad7e
Sebastien Collette <sco@synapsis-group.com>
2022-07-12 15:33:14 +0200

    Faster.

--- install.sh
+++ install.sh
@@ -8,29 +8,9 @@ export USERNAME=`whoami`
 sudo apt-get update
 sudo apt-get -y install --no-install-recommends apt-utils dialog 2>&1
 sudo apt-get install -y \
-  curl \
-  git \
-  gnupg2 \
-  jq \
-  sudo \
-  openssh-client \
-  less \
-  iproute2 \
-  procps \
-  wget \
-  unzip \
-  apt-transport-https \
-  lsb-release\
   fonts-firacode
 
 sudo fc-cache -f -v
 
 chsh -s /usr/bin/zsh $USERNAME
-
-# Cleanup
-sudo apt-get autoremove -y
-sudo apt-get autoremove -y
-sudo rm -rf /var/lib/apt/lists/*
-
-
 cp machine-settings.json /home/codespace/.vscode-remote/data/Machine/settings.json
