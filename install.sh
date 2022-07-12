c2e82ed3a927bc275370041941768f39884c7ce9
Sebastien Collette <sco@synapsis-group.com>
2022-07-12 14:25:02 +0200

    Simple install

--- install.sh
+++ install.sh
@@ -2,31 +2,5 @@
 
 sudo apt-get install fonts-firacode -y
 
-dotnet dev-certs https --trust
 
-create_symlinks() {
-    # Get the directory in which this script lives.
-    script_dir=$(dirname "$(readlink -f "$0")")
-
-    # Get a list of all files in this directory that start with a dot.
-    files=$(find -maxdepth 1 -type f -name ".*")
-
-    # Create a symbolic link to each file in the home directory.
-    for file in $files; do
-        name=$(basename $file)
-        echo "Creating symlink to $name in home directory."
-        rm -rf ~/$name
-        ln -s $script_dir/$name ~/$name
-    done
-}
-
-create_symlinks
-
-
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -R /root/.oh-my-zsh /home/$USERNAME
-cp /root/.zshrc /home/$USERNAME
-sed -i -e "s//root/.oh-my-zsh//home/$USERNAME/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
 
