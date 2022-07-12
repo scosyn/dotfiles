84994cbe261191b269738560ece8d29c67fe8c8c
Sebastien Collette <sco@synapsis-group.com>
2022-07-12 15:35:27 +0200

    Even faster.

--- install.sh
+++ install.sh
@@ -4,9 +4,6 @@ export DEBIAN_FRONTEND=noninteractive
 export INSTALL_ZSH=true
 export USERNAME=`whoami`
 
-## update and install required packages
-sudo apt-get update
-sudo apt-get -y install --no-install-recommends apt-utils dialog 2>&1
 sudo apt-get install -y \
   fonts-firacode
 
